4603e9b0295fb9c3ecfbd388eaac5f542734beab
Ykolo <loiclau2005@gmail.com>
2025-10-31 00:31:05 +0100

    Enhance AbstractFileReader and InterfaceFileReader with additional methods and implement file reading functionality

--- readers/AbstractFileReader.java
+++ readers/AbstractFileReader.java
@@ -1,7 +1,33 @@
 package fichier.readers;
 
+import java.io.File;
+import java.util.Scanner;
+
 public abstract class AbstractFileReader implements InterfaceFileReader {
+  protected String content = "";
+
   @Override
   public void readFile(String path) {
+    System.out.println("Reading file at path: " + path);
+    Scanner scanner = new Scanner(System.in);
+    File file = new File(path);
+
+    scanner.close();
+  }
+
+  @Override
+  public void showContent() {
+    System.out.println("Le contenue du fichier:");
+    System.out.println(content);
+  }
+
+  @Override
+  public void reverseContent() {
+    System.out.println("Le contenu du fichier a été inversé:");
+  }
+
+  @Override
+  public void palindromeContent() {
+    System.out.println("Le contenu du fichier est un palindrome:");
   }
 }
--- readers/InterfaceFileReader.java
+++ readers/InterfaceFileReader.java
@@ -3,4 +3,9 @@ package fichier.readers;
 public interface InterfaceFileReader {
   void readFile(String path);
 
+  void showContent();
+
+  void reverseContent();
+
+  void palindromeContent();
 }
